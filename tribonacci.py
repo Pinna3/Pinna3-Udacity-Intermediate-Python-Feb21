974fb1bb558c0151b9992aca1224645a201a795f
Michael-Pinna <mikepinna3@gmail.com>
2021-02-22 22:23:16 -0500

    is_tribonacci(num)

--- tribonacci.py
+++ tribonacci.py
@@ -1,12 +1,16 @@
 def generate_tribonacci_numbers():
     a, b, c = 0, 0, 1
-    while True: #1, 2, 4, 7, 13, 24, 44, 81
+    while True:
         a, b, c = b, c, a + b + c
         yield a
 
-# def is_tribonacci(num):
-#     """Return whether `num` is a Tribonacci number."""
-#     # Be careful to not loop infinitely!
-#     return False
+def is_tribonacci(num):
+    """Return whether `num` is a Tribonacci number."""
+    for trib in generate_tribonacci_numbers():
+        if num == trib:
+            return True
+        elif trib > num:
+            return False
 
-a = generate_tribonacci_numbers()
+print(is_tribonacci(24))
+print(is_tribonacci(25))
