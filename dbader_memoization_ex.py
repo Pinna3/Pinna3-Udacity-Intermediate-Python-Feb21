36891fe358bec72189c3578625baa245eaf87e5f
Michael Pinna <mikepinna3@gmail.com>
2021-02-26 18:28:46 -0500

    memoize.py

---
+++ dbader_memoization_ex.py
@@ -0,0 +1,18 @@
+def memoize(func):
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
--- memoize.py
+++ memoize.py
@@ -1,13 +1,36 @@
 import functools
-
+import time
 
 def memoize(function):
+    function.__cache__ = {}
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
-        return function(*args, **kwargs)
+        if (args, tuple(kwargs.items())) in function.__cache__.keys():
+            return function.__cache__[(args, tuple(kwargs.items()))]
+        else:
+            function.__cache__[(args, tuple(kwargs.items()))] = function(*args, **kwargs)
+            return function(*args, **kwargs)
     return wrapper
 
 @memoize
 def long_operation(x, y):
-    time.sleep(5)   # Or some other suitable long expression.
+    time.sleep(3)   # Or some other suitable long expression.
     return x + y
+
+@memoize
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+if __name__ == '__main__':
+    print(long_operation(2, 10))
+    print(long_operation(2, 10))
+    print(long_operation(2, 11))
+    print(long_operation(2, 11))
+    print(long_operation(2, 11))
+    print(long_operation(2, 11))
+    print(long_operation(1, 10))
+    print(long_operation(1, 10))
+    print(long_operation(1, 10))
+    print(long_operation(1, 10))
