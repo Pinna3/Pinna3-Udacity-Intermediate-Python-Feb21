862d931767d9d11f7769f62d02a6ac7eb2d793d4
Michael Pinna <mikepinna3@gmail.com>
2021-03-05 18:40:29 -0500

    Instructor step one, set up classes

---
+++ online_shopping_instructor/product.py
@@ -0,0 +1,10 @@
+class Product:
+    def __init__(self, name, description, price, seller, available=True):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.seller = seller
+        self.available = available
+        self. reviews = []
+
+        
