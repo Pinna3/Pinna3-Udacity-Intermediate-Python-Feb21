1e251ba3cdf48646620a37cbf6351b61ab8567ec
Michael Pinna <mikepinna3@gmail.com>
2021-03-06 17:25:28 -0500

    Appended instructors solution to file

--- Count hamelet words/count.py
+++ Count hamelet words/count.py
@@ -19,3 +19,17 @@ def count_unique_words(filename):
 
 if __name__ == '__main__':
     count_unique_words('hamlet.txt')
+
+
+
+# # Instructor's solution... (update() method looks a lot cleaner, also looping through lines)
+# def count_unique_words(filename='hamlet.txt'):
+#     words = collections.Counter()
+#     # Extract the data into Python.
+#     with open(filename) as f:
+#         for line in f:
+#             words.update(line.split())
+#
+#     # Calculate the ten most common words.
+#     for word, count in words.most_common(10):
+#         print(word, count)
